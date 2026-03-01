dce25fb9ae9e339cedc8b9e3bb747b6ae14e3868
Jiwoo Seo <ji2003woo@sookmyung.ac.kr>
2026-03-01 17:07:55 +0900

    Initial Commit

---
+++ acai bowl/acai_bowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  acai_bowlApp.swift
+//  acai bowl
+//
+//  Created by 서지우 on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct acai_bowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
